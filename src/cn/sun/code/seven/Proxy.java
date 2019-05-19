0265a8f7700b2266237740dddfcfcad09666d830
rider <463327005@qq.com>
2019-05-19 21:48:15 +0800

    代理模式

---
+++ src/cn/sun/code/seven/Proxy.java
@@ -0,0 +1,53 @@
+package cn.sun.code.seven;
+
+/**
+ * Subject类，定义了RealSubject和Proxy的公共接口，这样就在任何使用RealSubject的地方都可以
+ * 使用Proxy
+ */
+abstract class Subject {
+
+	public abstract void request();
+
+}
+
+/**
+ * RealSubject类，定义Proxy所代表的真实实体
+ */
+class RealSubject extends Subject {
+
+	@Override
+	public void request() {
+		System.out.println("真实的请求");
+	}
+}
+
+/**
+ * Proxy类，保存一个引用使得代理可以访问实体，并提供一个与Subject的接口相同的接口
+ * 这样代理就可以用来代替实体
+ */
+public class Proxy extends Subject {
+
+	private RealSubject realSubject;
+
+	@Override
+	public void request() {
+		// 可以在这里给被代理对象定制一些特性
+		if (realSubject == null) {
+			realSubject = new RealSubject();
+		}
+		realSubject.request();
+	}
+
+}
+
+/**
+ * 客户端代码
+ */
+class Client {
+
+	public static void main(String[] args) {
+		Proxy proxy = new Proxy();
+		proxy.request();
+	}
+
+}
