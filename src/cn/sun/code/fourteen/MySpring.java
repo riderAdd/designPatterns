670f9095b32a1bd66aed7545eda2a9844a8885db
rider <463327005@qq.com>
2019-05-23 08:43:48 +0800

    观察者模式2

---
+++ src/cn/sun/code/fourteen/MySpring.java
@@ -0,0 +1,17 @@
+package cn.sun.code.fourteen;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * 自定义观察者
+ */
+public class MySpring implements Observer {
+
+
+
+	@Override
+	public void update(Observable o, Object arg) {
+
+	}
+}
